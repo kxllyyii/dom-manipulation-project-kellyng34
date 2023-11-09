436c2455619791e8b5bfb642ebc7d211146341d5
kxllyyii <125307655+kxllyyii@users.noreply.github.com>
2023-11-08 19:14:10 -0500

    wf
    wf

--- app.js
+++ app.js
@@ -2,54 +2,52 @@
 
 const DOMSelectors = {
     form: document.querySelector("#form"),
-    firstName: document.querySelector(".firstName"),
-    lastName: document.querySelector(".lastName"),
-    pic: document.querySelector(".pic"),
+    firstName: document.querySelector("#firstName"),
+    lastName: document.querySelector("#lastName"),
+    pic: document.querySelector("#pic"),
     container: document.querySelector(".container"),
 };
 
   DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
-    const firstName = DOMSelectors.firstName.value
-    const lastName = DOMSelectors.lastName.value
-    const pic = DOMSelectors.pic.value
 
-    function divCreator(){
+    const food= {
+     firstName : DOMSelectors.firstName.value,
+     lastName : DOMSelectors.lastName.value,
+     pic : DOMSelectors.pic.value,
+
+    }
+    divCreator(food);
+    clear();
+    remove();
+  });
+
+
+    function divCreator(apple){
         DOMSelectors.container.insertAdjacentHTML(
             "afterbegin",
-            `<div class="container">
-              <div class="card">
-                ${firstName}
-                <p>Your Food</p>
-                <img src="${pic}" alt="" class="cardimg">
-                ${lastName}
+            `<div class="card">
+                <h2>${apple.firstName}</h2>
+                <h2> ${apple.lastName}</h2>
+                <h4>Your Food</h4>
+                <img src="${apple.pic}" alt="" class="cardimg">
                 <button class="button">Remove</button>
-              </div>
             </div>`
             )};
-          divCreator();
-        
-        }
-    );
+      
 
-function clear(){
-        DOMSelectors.form.addEventListener("submit", function(event){
-          event.preventDefault();
+  function clear(){
           DOMSelectors.firstName.value = "";
           DOMSelectors.lastName.value= "";
           DOMSelectors.pic.value= "";
-     });
-    }
-    clear();
+  };
+
 
 function remove() {
-  const buttons = document.querySelectorAll(".button");
-  buttons.forEach((button) => {
+  document.querySelectorAll(".button").forEach((button) => {
     button.addEventListener("click", function (event) {
-      const objectToRemove = event.target.closest(".card");
-      objectToRemove.remove();
+      event.target.parentElement.remove();
+    
     });
   });
 }
-
-remove();
